a95a82356a19517c12b445d8d802214238f10f32
darkm <darkmarty1912@gmail.com>
2023-10-19 00:06:29 +0300

    Initial commit

---
+++ src/main/java/com/example/cackesito/CackesitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cackesito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CackesitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CackesitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cackesito/CackesitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cackesito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CackesitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
